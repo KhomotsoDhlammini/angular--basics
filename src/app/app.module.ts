fa42943b72f13209e49ca80501bc4d0778c26e5f
khomotso <kgomotso_15@live.com>
2023-11-21 15:25:04 +0200

    fhrned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
+
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +15,7 @@ import { HeroDetailsComponent } from './hero-details/hero-details.component';
     HeaderComponent,
     BodyComponent,
     HeroesComponent,
-    HeroDetailsComponent
+    
   ],
   imports: [
     BrowserModule,
